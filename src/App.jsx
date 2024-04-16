f7f411b3b7d500c6f8abb01d00616c98ce8e5a63
Abhinav Upadhyay <abhinavupadhyay822@gmail.com>
2024-04-16 15:32:39 +0530

    functionalities added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useCallback, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useCallback, useEffect, useRef, useState } from "react";
+
 
 function App() {
   const [length, setLength] = useState(8);
   const [numbers, setNumbers] = useState(false);
   const [character, setCharacter] = useState(false);
   const [password, setPassword] = useState();
+
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -24,21 +24,36 @@ function App() {
       pass += str.charAt(char);
     }
     setPassword(pass);
-  }, [length, numbers, character, setPassword]);
+  }, [length, numbers, character]);
+
+  const onCopy = useCallback(() => {
+    passwordRef.current?.select()
+      window.navigator.clipboard.writeText(password)
+    }, [password]);
+  
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numbers, character, passwordGenerator]);
 
   return (
     <div className="text-center mt-20 m-auto w-7/12 p-16 shadow-xl rounded-3xl text-white text-2xl bg-gray-800">
       <h1 className="text-6xl font-bold ">Password Generator</h1>
       <div className="mt-8">
-        <div className="flex mb-4 gap-2">
+        <div className="flex mb-4 gap-0">
           <input
             type="text"
             value={password}
-            className="outline-none rounded-xl w-full py-1 px-3 bg-gray-100"
+            className="outline-none rounded-l-xl w-full py-1 px-3 bg-gray-100 text-black"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="text-xl text-semibold outline-none bg-blue-700 text-white p-2 shrink-0 rounded-xl">
+          <button
+            onClick={onCopy}
+            
+            className="text-xl font-semibold outline-none bg-blue-700 text-white p-2 px-4 shrink-0 rounded-r-xl hover:bg-blue-600"
+          >
             Copy
           </button>
         </div>
@@ -61,23 +76,29 @@ function App() {
               type="checkbox"
               defaultChecked={setNumbers}
               id="numberInput"
+              className="cursor-pointer"
               onChange={() => {
                 setNumbers((prev) => !prev);
               }}
             />
-            <label>Number</label>
+            <label>Numbers</label>
           </div>
           <div className="flex gap-2">
             <input
               type="checkbox"
               defaultChecked={setCharacter}
               id="charInput"
+              className="cursor-pointer"
               onChange={() => {
                 setCharacter((prev) => !prev);
               }}
             />
-            <label>Character</label>
+            <label>Characters</label>
           </div>
+          <button onClick={passwordGenerator} className="cursor-pointer">
+            <i className="ri-loop-right-line mr-2"></i>
+            Regenerate Password
+          </button>
         </div>
       </div>
     </div>
